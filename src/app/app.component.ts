15ac2d8f823a7a326e0244701a72c11a99ee9593
karlral <karlral@gmail.com>
2025-02-20 01:27:47 -0300

    Seguridad de Acceso

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,52 +6,30 @@ import { SystemService } from './service/system.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'URCI';
-  regional:Regional={
-    idregional: 0,
-    nomregional: '',
-    nomcorto: '',
-    telefono: '',
-    direccion: '',
-    email: '',
-    ano: 0,
-    presentacion: '',
-    logo: ''
-  };
-  constructor(private regionalService:RegionalService,
-    private systemService:SystemService
-  ){
-    
-  }
+  regional: any = {};
+  constructor(
+    private regionalService: RegionalService,
+    private systemService: SystemService
+  ) {}
   ngOnInit(): void {
-    console.log(this.title+': '+system);
-    this.regionalService.obtenerRegionalPub(system).subscribe(
-      {
+    // console.log(this.title+': '+system);
+    this.regional = this.systemService.getSystem();
+    if (this.regional == null) {
+      this.regionalService.obtenerRegionalPub(system).subscribe({
         next: (dato: any) => {
-          this.regional=dato;
-          
+          this.regional = dato;
+
           this.systemService.setSystem(this.regional);
-          this.title=this.regional.nomcorto;
-          if(system==2){
-            this.changeBackgroundImage();
-          }
-          
         },
         error: (error) => {
           console.log(error);
         },
-        complete: () => console.info('completo carga de regionales')
+        complete: () => console.info('completo carga de regionales'),
       });
-      
-  }
-
-  changeBackgroundImage() {
-    document.getElementById("fondo")?.classList.remove("bgScroll");
-    document.getElementById("fondo")?.classList.add("bgScroll2");
-    document.getElementById("trasparencia")?.classList.remove("opacity7");
-    document.getElementById("trasparencia")?.classList.add("opacity3");
+    }
   }
 }
--- src/app/service/normal.guard.ts
+++ src/app/service/normal.guard.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { LoginService } from './login.service';
+import system from './helpersys';
 
 @Injectable({
   providedIn: 'root'
@@ -14,7 +15,7 @@ export class NormalGuard implements CanActivate {
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-      if(this.loginService.isLoggedIn() && this.loginService.getUserRole() == 'NORMAL'){
+      if(this.loginService.isLoggedIn() && this.loginService.getUserRole() == 'NORMAL' && this.loginService.getUserIdregional() == system){
         return true;
       }else{
         this.router.navigate(['sistema']);
