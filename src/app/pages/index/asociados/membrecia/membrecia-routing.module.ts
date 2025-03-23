8a311b3a1d711beffbe7589755d3a1f7fd82544f
karlral <karlral@gmail.com>
2025-03-22 23:48:41 -0400

    fix membresia modular

--- src/app/pages/index/asociados/membrecia/membrecia-routing.module.ts
+++ src/app/pages/index/asociados/membrecia/membrecia-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MembreciaComponent } from './membrecia.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: MembreciaComponent,
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
