e357f830ba77ee59fdd37af2a664c38f35914253
karlral <karlral@gmail.com>
2025-03-26 10:47:17 -0300

    fix fecmodi fix foto

--- src/app/pages/admin/view/corredor/add-edit-corredor/add-edit-corredor.component.ts
+++ src/app/pages/admin/view/corredor/add-edit-corredor/add-edit-corredor.component.ts
@@ -391,6 +391,7 @@ modalidad:Modalidad={
     }
     this.corredorForm.get('usuario')?.setValue(this.usuario);
     this.corredorForm.get('regional')?.setValue(this.regional);
+    this.corredorForm.get('fecmodi')?.setValue(this.fecha);
    // console.log(this.corredorForm.value);
 
     if (this.selectedCorredor) {
