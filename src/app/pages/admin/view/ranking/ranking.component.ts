984bb4886b50779f17df9bd018c04970fd90a1e3
karlral <karlral@gmail.com>
2026-02-19 21:31:12 -0300

    informe ranking

--- src/app/pages/admin/view/ranking/ranking.component.ts
+++ src/app/pages/admin/view/ranking/ranking.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import * as FileSaver from 'file-saver';
+import { Table } from 'primeng/table';
 import { ConfirmationService, MessageService } from 'primeng/api';
 import { Corredorank } from 'src/app/domain/custom/corredorank';
 import { Movimiento } from 'src/app/domain/movimiento';
@@ -12,6 +14,8 @@ import { MovimientoService } from 'src/app/service/movimiento.service';
   providers: [MessageService,ConfirmationService]
 })
 export class RankingComponent  implements OnInit {
+
+  @ViewChild('dt') table!: Table;
   
   corredorankes: Corredorank[] = [];
   corredorank: Corredorank={
@@ -28,9 +32,14 @@ export class RankingComponent  implements OnInit {
     puntua: 0,
     cantidad: 0
   }; 
+  
+  corredorankes2: Pick<Corredorank,  'nomconcepto' |  'corredor' | 'club' | 'categoria' | 'entrada' >[] = [];
+
   displayAddModal=false;
   entradas:number=0;
   salidas:number=0;
+
+   
 
   constructor( private messageService: MessageService,
     private movimientoService: MovimientoService,
@@ -166,6 +175,53 @@ desactivaPuntuaCorredor(idcorredor:any){
     }
   );
 }
+
+exportExcel2() {
+  
+  if (this.table.filteredValue) {
+    this.corredorankes2 = this.table.filteredValue
+      .map(({  nomconcepto, corredor, club, categoria, entrada })  => ({  nomconcepto, corredor, club, categoria, entrada }))
+      .sort((a, b) => a.club.localeCompare(b.club)); 
+  }else {
+    this.corredorankes2 = this.corredorankes
+      .map(({  nomconcepto, corredor, club, categoria, entrada })  => ({  nomconcepto, corredor, club, categoria, entrada }))
+      .sort((a, b) => a.club.localeCompare(b.club)); 
+  }
+
+
+
+    import('xlsx').then((xlsx) => {
+      const worksheet = xlsx.utils.json_to_sheet(
+        this.corredorankes2
+      );
+      const workbook = {
+        Sheets: { data: worksheet },
+        SheetNames: ['data'],
+      };
+      const excelBuffer: any = xlsx.write(workbook, {
+        bookType: 'xlsx',
+        type: 'array',
+      });
+      this.saveAsExcelFile(excelBuffer, 'participantes');
+    });
+  }
+
+    saveAsExcelFile(buffer: any, fileName: string): void {
+      let EXCEL_TYPE =
+        'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
+      let EXCEL_EXTENSION = '.xlsx';
+      const data: Blob = new Blob([buffer], {
+        type: EXCEL_TYPE,
+      });
+      FileSaver.saveAs(
+        data,
+        fileName + '_' + new Date().getTime() + EXCEL_EXTENSION
+      );
+    }
+
+    
+
+
 
 }
 
